5d944e1fcb30808eae3c38d106a12d2334bc45c8
Bartosz Kozielski <bartoszkozielski1998@gmail.com>
2023-12-20 00:39:44 +0100

    commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,51 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+galleryItems.forEach((image) => {
+  const { preview, original, description } = image;
+
+  const li = document.createElement("li");
+  li.classList.add("gallery__item");
+
+  const link = document.createElement("a");
+  link.classList.add("gallery__link");
+  link.href = original;
+
+  const img = document.createElement("img");
+  img.classList.add("gallery__image");
+  img.src = preview;
+  img.dataset.source = original;
+  img.alt = description;
+
+  link.appendChild(img);
+  li.appendChild(link);
+  gallery.appendChild(li);
+
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const instance = basicLightbox.create(
+      `
+      <img src="${
+        event.currentTarget.querySelector("img").dataset.source
+      }" alt="${description}">
+    `,
+      {
+        onShow: (instance) => {
+          document.addEventListener("keydown", (e) => {
+            if (e.key === "Escape") {
+              instance.close();
+            }
+          });
+        },
+      }
+    );
+
+    instance.show();
+  });
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,36 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const gallery = document.querySelector(".gallery");
+
+
+galleryItems.forEach((item) => {
+  const { original, preview, description } = item;
+
+  const li = document.createElement("li");
+  li.classList.add("gallery__item");
+
+  const a = document.createElement("a");
+  a.classList.add("gallery__link");
+  a.href = original;
+
+  const img = document.createElement("img");
+  img.classList.add("gallery__image");
+  img.src = preview;
+  img.alt = description;
+
+  a.appendChild(img);
+  li.appendChild(a);
+  gallery.appendChild(li);
+});
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+    captionsDataAlt: "Image description",
+  });
+});
